b0c4223cc884e75fe408b9c5c4fc6ba45d2b993b
Satyamev Singh <135261227+Satyamev27016@users.noreply.github.com>
2025-04-05 00:10:06 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import 'bootstrap/dist/js/bootstrap.min.js';
+
+//import NavBar from "./components/Navbar.js"
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    </div>
+  );
+}
+
+export default App;
